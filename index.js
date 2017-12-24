9bb33159adedf8c312ee021131f8d9427dd9d541
Sergio Rios <sergiorios@Sergios-MacBook-Air.local>
2017-12-23 23:35:19 -0800

    js modification

--- index.js
+++ index.js
@@ -197,7 +197,7 @@
 var canvasDiv = document.getElementById('particle-canvas');
 var options = {
     particleColor: '#888',
-    background: 'https://raw.githubusercontent.com/JulianLaval/canvas-particle-network/master/img/demo-bg.jpg',
+    //background: 'https://raw.githubusercontent.com/JulianLaval/canvas-particle-network/master/img/demo-bg.jpg',
     interactive: true,
     speed: 'medium',
     density: 'high'
